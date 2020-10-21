0308264004294bad672a176d2151c2ace078aa48
Katie <25576005+stuttskl@users.noreply.github.com>
2020-10-21 15:20:50 -0700

    updated to use clear db

--- api/dbcon.js
+++ api/dbcon.js
@@ -3,11 +3,18 @@ dotenv.config();
 
 let mysql = require('mysql');
 
+// let pool = mysql.createConnection({
+//   host: process.env.DATABASE_HOST,
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DATABASE
+// });
+
 let pool = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE
+  host: process.env.CLEAR_DATABASE_HOST,
+  user: process.env.CLEAR_DATABASE_USER,
+  password: process.env.CLEAR_DATABASE_PASSWORD,
+  database: process.env.CLEAR_DATABASE_NAME
 });
 
 pool.connect(function(err) {
