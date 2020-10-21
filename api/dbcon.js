56a356cd4ddaedee60feda3366ef0d45de1d9b49
Katie <25576005+stuttskl@users.noreply.github.com>
2020-10-21 14:27:50 -0700

    added webpack merge for prod deployment

--- api/dbcon.js
+++ api/dbcon.js
@@ -4,14 +4,14 @@ dotenv.config();
 let mysql = require('mysql');
 
 let pool = mysql.createConnection({
-  HOST: "us-cdbr-east-02.cleardb.com",
-  USER: "b3c748fa70f92b",
-  PASSWORD: "b8571cfc",
-  DB: "heroku_dcebc5285cd0a08"
-  // host: process.env.DATABASE_HOST,
-  // user: process.env.DATABASE_USER,
-  // password: process.env.DATABASE_PASSWORD,
-  // database: process.env.DATABASE
+  // HOST: "us-cdbr-east-02.cleardb.com",
+  // USER: "b3c748fa70f92b",
+  // PASSWORD: "b8571cfc",
+  // DB: "heroku_dcebc5285cd0a08"
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE
 });
 
 pool.connect(function(err) {
