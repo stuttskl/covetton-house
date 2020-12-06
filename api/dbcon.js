791832599ddb1fb02ac3196c8af765b9d97fe26a
Katie <25576005+stuttskl@users.noreply.github.com>
2020-12-06 12:40:49 -0800

    Update dbcon.js

--- api/dbcon.js
+++ api/dbcon.js
@@ -4,10 +4,6 @@ dotenv.config();
 let mysql = require('mysql');
 
 let pool = mysql.createConnection({
-  // HOST: "us-cdbr-east-02.cleardb.com",
-  // USER: "b3c748fa70f92b",
-  // PASSWORD: "b8571cfc",
-  // DB: "heroku_dcebc5285cd0a08"
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
@@ -22,5 +18,3 @@ pool.connect(function(err) {
 });
 
 module.exports.pool = pool;
-
-// mysql://b3c748fa70f92b:b8571cfc@us-cdbr-east-02.cleardb.com/heroku_dcebc5285cd0a08?reconnect=true
